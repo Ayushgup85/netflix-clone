fa71f317bdb6f08e2b8d8346df30ecceacfd7a50
Ayush <ag7159441@gmail.com>
2021-05-13 17:19:46 +0530

    netflix-clone

---
+++ src/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './Nav'
+import Banner from './Banner'
+import "./HomeScreen.css"
+const HomeScreen = () => {
+    return (
+        <div className="homeScreen">
+            <Nav/>
+           <Banner/>
+        </div>
+
+    )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAW4bNugyEqB53PjgAM-Wj4VZ09w2sg3mQ",
+  authDomain: "netflix-clone-13235.firebaseapp.com",
+  projectId: "netflix-clone-13235",
+  storageBucket: "netflix-clone-13235.appspot.com",
+  messagingSenderId: "125353994299",
+  appId: "1:125353994299:web:00ec71c16061e8a0f4141e"
+};
